045b40c2a3c8bcd70c64d98e85c562b4f88a91d8
Tri Trinh <minhtrizs2000@gmail.com>
2021-03-14 00:34:04 +0700

    fix responsive

--- JS/main.js
+++ JS/main.js
@@ -3,3 +3,4 @@ document.getElementById("airBagDisclosures").onclick = function(){
     document.getElementById("airBagDisclosures").classList.toggle("arrowUp");
     document.getElementById("airBag").classList.toggle("airbag__long");
 }
+
