03b80702e0e911b1e0e2101ecc6606b6a291366f
Tri Trinh <minhtrizs2000@gmail.com>
2021-03-13 21:54:22 +0700

    first upload (ford.com)

---
+++ JS/main.js
@@ -0,0 +1,5 @@
+document.getElementById("airBagDisclosures").onclick = function(){
+    document.getElementById("airBagDisclosures").classList.toggle("arrowDown");
+    document.getElementById("airBagDisclosures").classList.toggle("arrowUp");
+    document.getElementById("airBag").classList.toggle("airbag__long");
+}
